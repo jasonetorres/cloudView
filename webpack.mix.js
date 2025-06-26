8946d639f3acd53caf650a96785a57fb1177fcb7
jasonetorres <jasontorres585@icloud.com>
2025-06-26 14:01:51 -0400

    routing

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.js('resources/js/bootstrap.js', 'public/js')
+mix.js('public/js/bootstrap.js', 'public/js')
    .react() // Chain .react() here
-   .postCss('resources/css/app.css', 'public/css', [
+   .postCss('public/css/app.css', 'public/css', [
        tailwindcss('./tailwind.config.js'),
        require('autoprefixer'),
    ])
